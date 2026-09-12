e588be74378a0acce345f47d3bf32c0dfd3e35a2
fodpedal1-cmyk <fodpedal1@gmail.com>
2026-09-12 09:02:30 +0800

    Update PluginProcessor.h

--- Source/PluginProcessor.h
+++ Source/PluginProcessor.h
@@ -2,45 +2,25 @@
 
 #include <JuceHeader.h>
 
-//==============================================================
-// RG BLUE DELAY
-// Blue Ocean / Deep Blue inspired delay DSP
-// 3 controls: DELAY / REPEAT / MIX
-//==============================================================
+class RGBlueDelayAudioProcessorEditor;
 
 class RGBlueDelayAudioProcessor : public juce::AudioProcessor
 {
 public:
-
     RGBlueDelayAudioProcessor();
     ~RGBlueDelayAudioProcessor() override;
 
-    //==========================================================
-    // AudioProcessor
-    //==========================================================
-
-    void prepareToPlay(double sampleRate,
-                       int samplesPerBlock) override;
-
+    void prepareToPlay(double sampleRate, int samplesPerBlock) override;
     void releaseResources() override;
 
-    bool isBusesLayoutSupported(
-        const BusesLayout& layouts) const override;
+    bool isBusesLayoutSupported(const BusesLayout& layouts) const override;
 
     void processBlock(
         juce::AudioBuffer<float>&,
         juce::MidiBuffer&) override;
 
-    //==========================================================
-    // Editor
-    //==========================================================
-
     juce::AudioProcessorEditor* createEditor() override;
     bool hasEditor() const override;
-
-    //==========================================================
-    // Plugin information
-    //==========================================================
 
     const juce::String getName() const override;
 
@@ -49,10 +29,6 @@ public:
     bool isMidiEffect() const override;
 
     double getTailLengthSeconds() const override;
-
-    //==========================================================
-    // Programs
-    //==========================================================
 
     int getNumPrograms() override;
     int getCurrentProgram() override;
@@ -63,10 +39,6 @@ public:
         int index,
         const juce::String& newName) override;
 
-    //==========================================================
-    // State
-    //==========================================================
-
     void getStateInformation(
         juce::MemoryBlock& destData) override;
 
@@ -74,69 +46,108 @@ public:
         const void* data,
         int sizeInBytes) override;
 
-    //==========================================================
-    // Parameters
-    //==========================================================
-
     juce::AudioProcessorValueTreeState parameters;
+
+private:
+    struct RCFilter
+    {
+        float a = 0.0f;
+        float b = 0.0f;
+        float z = 0.0f;
+
+        void reset()
+        {
+            z = 0.0f;
+        }
+
+        void setLowPass(
+            double sampleRate,
+            float cutoff);
+
+        void setHighPass(
+            double sampleRate,
+            float cutoff);
+
+        float process(float input);
+    };
+
+    struct DelayLine
+    {
+        juce::AudioBuffer<float> buffer;
+        int size = 0;
+        int writePosition = 0;
+
+        void prepare(
+            int channels,
+            int samples);
+
+        void clear();
+
+        void write(
+            int channel,
+            float value);
+
+        float read(
+            int channel,
+            float delaySamples) const;
+
+        void advance();
+    };
+
+    struct ChannelState
+    {
+        RCFilter inputHP;
+        RCFilter inputLP;
+
+        RCFilter delayHP;
+        RCFilter delayLP;
+
+        RCFilter feedbackHP;
+        RCFilter feedbackLP;
+
+        RCFilter outputLP;
+
+        float feedbackState = 0.0f;
+        float delayClock = 0.0f;
+
+        void reset();
+    };
 
     static juce::AudioProcessorValueTreeState::ParameterLayout
     createParameterLayout();
 
-private:
+    void resetDSP();
 
-    //==========================================================
-    // Component-inspired DSP
-    //==========================================================
-
-    juce::dsp::IIR::Filter<float> inputHighPass;
-    juce::dsp::IIR::Filter<float> inputLowPass;
-
-    juce::dsp::IIR::Filter<float> feedbackHighPass;
-    juce::dsp::IIR::Filter<float> feedbackLowPass;
-
-    juce::dsp::IIR::Filter<float> outputLowPass;
-
-    //==========================================================
-    // PT2399-style delay line
-    //==========================================================
-
-    juce::AudioBuffer<float> delayBuffer;
-
-    int delayBufferSize = 0;
-    int writePosition = 0;
-
-    float currentDelaySamples = 0.0f;
-
-    //==========================================================
-    // Smoothed parameters
-    //==========================================================
-
-    juce::SmoothedValue<float> delaySmoothed;
-    juce::SmoothedValue<float> feedbackSmoothed;
-    juce::SmoothedValue<float> mixSmoothed;
-
-    juce::SmoothedValue<float> bypassSmoothed;
-
-    //==========================================================
-    // Internal state
-    //==========================================================
-
-    double currentSampleRate = 44100.0;
-
-    bool bypassState = false;
-
-    //==========================================================
-    // DSP helpers
-    //==========================================================
+    float processDelaySample(
+        int channel,
+        float input,
+        float delaySamples,
+        float feedback,
+        float mix);
 
     float readDelaySample(
         int channel,
         float delaySamples) const;
 
-    void updateFilters();
+    void updateParameters();
 
-    //==========================================================
+    juce::AudioBuffer<float> delayBuffer;
+
+    ChannelState channelState[2];
+
+    juce::SmoothedValue<float> delaySmoothed;
+    juce::SmoothedValue<float> feedbackSmoothed;
+    juce::SmoothedValue<float> mixSmoothed;
+    juce::SmoothedValue<float> bypassSmoothed;
+
+    double currentSampleRate = 44100.0;
+
+    int delayBufferSize = 0;
+    int writePosition = 0;
+
+    float delayTimeMs = 300.0f;
+    float feedbackAmount = 0.45f;
+    float mixAmount = 0.32f;
 
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(
         RGBlueDelayAudioProcessor)
